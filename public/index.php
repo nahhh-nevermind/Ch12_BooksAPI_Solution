c943d0bbfb17989db3c0759fc1a7e3ee596d5def
Jeremy Geafri <jeremy.geafri05@gmail.com>
2026-06-23 15:54:10 +0800

    Update on index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,15 @@ use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+header("Access-Control-Allow-Origin: https://ch13-full-stack-solution-85klzadqw-utms-tudent.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// Handle preflight OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
 require __DIR__ . '/../vendor/autoload.php';
 
 Dotenv::createImmutable(__DIR__ . '/..')->safeLoad();
